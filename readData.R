a531e4c75f6f4e0a40841067ed29ad0e116ae0a0
Irina Belyaeva <ibelyaev@umd.edu>
2014-04-26 20:24:43 -0400

    Updated readData.R script.

--- readData.R
+++ readData.R
@@ -1,10 +1,10 @@
 # Read datasets
 
-dataVariableNames <- read.table("./data/features.txt")
-dataActivityNames <- read.table("./data/activity_labels.txt")
-testSubject <- read.table("./data/test/subject_test.txt")
+featureData <- read.table("./data/features.txt")
+activitiesLabelData <- read.table("./data/activity_labels.txt")
+testSubjectData <- read.table("./data/test/subject_test.txt")
 testData <- read.table("./data/test/X_test.txt")
-testActivity <- read.table("./data/test/y_test.txt")
-trainSubject <- read.table("./data/train/subject_train.txt")
+testActivityData <- read.table("./data/test/y_test.txt")
+trainSubjectData <- read.table("./data/train/subject_train.txt")
 trainData <- read.table("./data/train/X_train.txt")
-trainActivity <- read.table("./data/train/y_train.txt")
+trainActivityData <- read.table("./data/train/y_train.txt")
